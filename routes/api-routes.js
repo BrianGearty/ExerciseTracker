c6ab23c1c01240c0be76e58a68b7022023d3b7c5
Brian GEARTY <geartybrian@gmail.com>
2020-10-27 21:25:39 -0400

    final commit

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,18 +6,26 @@ module.exports = function(app) {
     // PUT route for exercise data by ID
     app.put("/api/workouts/:id", function (req, res){
         console.log("PUT EXCERCISE BY ID ROUTE HIT")
-        db.Workout.findOneAndUpdate({_id: req.params._id})
+        var exerciseId = req.params.id;
+        console.log("req setup", req.params.id);
+        var exerciseData = req.body;
+        db.Workout.findByIdAndUpdate(
+            {_id: exerciseId},
+            {$push: {exercises: exerciseData}},
+            {new:true, runValidators: true}
+        )
         .then(data => {
             res.json(data);
         })
         .catch(err => {
+            console.log(err)
             res.json(err);
         });
     });
 
     // GET route for workouts data
     app.get("/api/workouts/", function(req, res){
-        console.log("GET EXERCISE DATA ROUTE HIT")
+        console.log("GET EXERCISE DATA ROUTE HIT", req.params)
         db.Workout.find()
         .then(data => {
             res.json(data);
@@ -29,9 +37,9 @@ module.exports = function(app) {
 
     // POST route for workouts data
     app.post("/api/workouts", function(req, res){
-        console.log("WORKOUT POST API ROUTE HIT")
+        console.log("WORKOUT POST API ROUTE HIT", req.body)
         db.Workout.create({})
-        .then(data => {
+            .then(data => {
             res.json(data);
         })
         .catch(err => {
@@ -41,11 +49,13 @@ module.exports = function(app) {
 
     // GET route for stats data
     app.get("/api/workouts/range", function(req, res){
+        ("STATS GET ROUTE HIT", req.body)
         db.Workout.find({}).limit(8)
         .then(data => {
             res.json(data);
         })
         .catch(err => {
+            console.log(err)
             res.json(err);
         });
     });
